5c6654cdcac5b7a81823a28f4ff66bbcfc64f1f2
Fkeito <1417089@ed.tus.ac.jp>
2018-06-16 21:47:19 +0900

    BuildSettings

---
+++ 3HoursChallengeProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	void Awake()
+    {
+        Input.gyro.enabled = true;
+    }
+	void Update () {
+        transform.rotation = Quaternion.AngleAxis(90.0f, Vector3.right) * Input.gyro.attitude * Quaternion.AngleAxis(180.0f, Vector3.forward);
+    }
+}
