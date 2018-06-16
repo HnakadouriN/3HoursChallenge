a17a87a27c7d004bc672005ccd8ac5c74997f5cb
Fkeito <1417089@ed.tus.ac.jp>
2018-06-16 20:51:15 +0900

    数字トリック用のさむしんぐ

---
+++ 3HoursChallengeProject/Assets/Scripts/Number.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Number : Stuff {
+
+    private Text text;
+    public int i;
+
+    void Start()
+    {
+        text = this.GetComponentInChildren<Text>();
+    }
+
+    public override void OnClickDown()
+    {
+        base.OnClickDown();
+        i = ++i % 10;
+        text.text = i.ToString();
+    }
+}
---
+++ 3HoursChallengeProject/Assets/Scripts/NumberTrickDemo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberTrickDemo : MonoBehaviour {
+
+    private Number[] numbers;
+    public int correctNum = 1111;
+
+	// Use this for initialization
+	void Start () {
+        numbers = this.GetComponentsInChildren<Number>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int i = 0;
+		foreach(Number num in numbers)
+        {
+            i = i * 10 + num.i;
+        }
+        if (i == correctNum)
+        {
+            Debug.Log("correct");
+            foreach (Number num in numbers) Destroy(num);
+            Destroy(this);
+        }
+	}
+}
