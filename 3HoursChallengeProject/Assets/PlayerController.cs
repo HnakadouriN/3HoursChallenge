1dad357a0aeaade9995b6d3fef03ec8d0c19680d
長谷部直起 <hasebenaoki@kaguwlan220235.ed.tus.ac.jp>
2018-06-16 18:16:00 +0900

    Playerの作成

---
+++ 3HoursChallengeProject/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private UnityEngine.UI.Image aim;
+    private GameObject mainCamera;
+    // Use this for initialization
+    void Start()
+    {
+        aim = this.GetComponent<UnityEngine.UI.Image>();
+        mainCamera = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Stuff stuff = hit.collider.gameObject.GetComponent<Stuff>();
+                if(stuff){
+                    stuff.OnClick();
+                }
+            }
+        }
+    }
+}
