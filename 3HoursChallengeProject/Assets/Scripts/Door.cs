243845c198bfc91f3f8c211a5e14aa18f753d932
Fkeito <1417089@ed.tus.ac.jp>
2018-06-16 19:06:33 +0900

    ドアつくった

---
+++ 3HoursChallengeProject/Assets/Scripts/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : Stuff {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public override void OnClickDown()
+    {
+        base.OnClickDown();
+        MoveRoom("DebugStuff");
+    }
+
+    private void MoveRoom(string roomName)
+    {
+        SceneManager.LoadScene(roomName);
+    }
+}
