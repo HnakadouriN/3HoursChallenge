cc5b488a3f862bee048bdc667af3197ac81fdb58
Fkeito <1417089@ed.tus.ac.jp>
2018-06-16 21:54:36 +0900

    罪の深さを反省します

--- 3HoursChallengeProject/Assets/Scripts/CameraController.cs
+++ 3HoursChallengeProject/Assets/Scripts/CameraController.cs
@@ -7,6 +7,9 @@ public class CameraController : MonoBehaviour {
 	void Awake()
     {
         Input.gyro.enabled = true;
+#if UNITY_EDITOR
+        Input.gyro.enabled = false;
+#endif
     }
 	void Update () {
         transform.rotation = Quaternion.AngleAxis(90.0f, Vector3.right) * Input.gyro.attitude * Quaternion.AngleAxis(180.0f, Vector3.forward);
