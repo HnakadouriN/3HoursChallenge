d43b512bb206aac70829e0afce67b0c06835e565
幡谷亮 <hatayaryo@kaguwlan221178.ed.tus.ac.jp>
2018-06-16 22:02:25 +0900

    ステージ２の追加

---
+++ 3HoursChallengeProject/Assets/Scripts/KeyScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : Stuff {
+
+	public  RodScript rodbool;
+	public bool KeyBool = false;
+
+	public override void OnClickDown ()
+	{
+		base.OnClickDown ();
+		//Debug.Log ("鍵だ");
+		if (rodbool.RodBool) {
+			Debug.Log ("「棒を使って鍵を入手した」");
+			KeyBool = true;
+			Destroy (this.gameObject);
+		} else {
+			Debug.Log ("「鍵に手が届かない」");
+			Debug.Log ("「何か長い物があれば取れそうだ」");
+		}
+	}
+}
---
+++ 3HoursChallengeProject/Assets/Scripts/RodScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RodScript : Stuff {
+
+	public bool RodBool = false;
+
+	public override void OnClickDown()
+	{
+		base.OnClickDown ();
+		Debug.Log ("「長い棒を入手した　何かに使えそうだ」");
+		RodBool = true;
+		Destroy (this.gameObject);
+	}
+
+}
