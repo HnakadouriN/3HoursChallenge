3587c50489814f5104b673a1dcd2cd6941ff0892
Moosan <washer2016@outlook.jp>
2018-06-18 16:35:56 +0900

    StuffSampleAdd

---
+++ 3HoursChallengeProject/Assets/Stuff/StuffExsamples/Gun/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Gun : Stuff {
+    private float clickTime;
+    private Vector3 startPos;
+    private void Start()
+    {
+        clickTime = 0;
+    }
+    public override void OnClickDown()
+    {
+        base.OnClickDown();
+        startPos = transform.position;
+    }
+    public override void OnClick()
+    {
+        clickTime += Time.deltaTime;
+        transform.eulerAngles += transform.up * clickTime;
+        transform.position = startPos;
+    }
+    public override void OnClickUp()
+    {
+        base.OnClickUp();
+        transform.GetComponent<Rigidbody>().AddForce(transform.forward.normalized * clickTime * 10 ,ForceMode.Impulse);
+        clickTime = 0;
+    }
+}
--- 3HoursChallengeProject/Assets/Stuff/StuffTest/DebugStuff.cs
+++ 3HoursChallengeProject/Assets/Stuff/StuffTest/DebugStuff.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DebugStuff : Stuff {
-    private int a = 0;
     public override void OnClickDown()
     {
         base.OnClickDown();
