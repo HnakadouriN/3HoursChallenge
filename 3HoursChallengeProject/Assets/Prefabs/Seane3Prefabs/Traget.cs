c41f5171ab25f9658135098f09b80766749617c7
長谷部直起 <hasebenaoki@hasebetadashikinoMacBook-ea.local>
2018-06-18 17:01:21 +0900

    途中経過

---
+++ 3HoursChallengeProject/Assets/Prefabs/Seane3Prefabs/Traget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traget : MonoBehaviour
+{
+
+    public TargetType targetType;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+public enum TargetType{
+    itemObject,
+    eventObject,
+    none,
+}
+
